e4dd8c64f3856fc29b9bf1bead4dcf28bc9a2a5e
Íñigo Aréjula Aísa <arejula27@gmail.com>
2019-07-28 15:44:35 +0200

    se ha creado el swiper con sus efectos en una pagina exclusiva

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/card_swiper.dart';
+
 
 class HomePage extends StatelessWidget {
-  
+  Container blueC = new Container(
+    color: Colors.blue,
+  );
+  Container yellowC = new Container(
+    color: Colors.yellow,
+  );
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){},
+
+          ),
+        ],
+        centerTitle: false,
         title: Text('Home Page'),
       ),
+      body: Container(
+        height: 350.0,
+        child: _swiperTarjetas(),
+      )
+      //body: Container(color: Colors.blue,child: Text('Hola'),),
+    
+    );
+  }
+  Widget _swiperTarjetas(){
+
+    return CardSwiper(
+      peliculas: [1,2,3,4,5],
     );
   }
 }
